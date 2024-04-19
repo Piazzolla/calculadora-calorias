04cc9dd543935600d3d5b5df1873842aa0a92a71
Mariano <macacozzz@hotmail.com>
2024-04-19 17:55:02 -0300

    upgraded to latest nexjs version -13.5-

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import { BudgetProvider } from 'context/budget';
+//import { Lato } from '@next/font/google'
+
+// const lato = Lato({ 
+//     weight: '400',
+//     style: 'normal',
+//     subsets: ['latin'] 
+// })
 
 function CalBudgetApp({ Component, pageProps }: AppProps) {
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import clientPromise from '../lib/mongodb'
 import { InferGetServerSidePropsType } from 'next'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export async function getServerSideProps() {
   try {
@@ -30,74 +32,97 @@ export default function Home({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
-    <div className={styles['super-container']}>
-      <header className={styles["primary-bg"]}>
-        <nav className={styles["nav"]}>
-          <ul className={styles["nav-ul"]}>
-            <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Home</a></li>
-            <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Features</a></li>
-            <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Pricing</a></li>
-            <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Contact</a></li>
-          </ul>
-        </nav>
-        <div className="hero">
-          <h1>Lose weight and stay healthy</h1>
-          <p>Our weight loss app helps you reach your goals and maintain a healthy lifestyle</p>
-          <button className={styles["primary-btn"]}>Get started</button>
-        </div>
-      </header>
-      <section className="secondary-bg">
-        <div className="container">
-          <h2 className="text-center">Our features</h2>
-          <div className="feature">
-            <img src="icon1.png" alt="Feature 1" className={styles["feature-icon"]} />
-            <div className="feature-content">
-              <h3>Track your progress</h3>
-              <p>Keep track of your weight, measurements, and body fat percentage with our app</p>
+      <div className={styles['super-container']}>
+        <header className={styles["primary-bg"]}>
+          <nav className={styles["nav"]}>
+            <ul className={styles["nav-ul"]}>
+              <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Home</a></li>
+              <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Features</a></li>
+              <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Pricing</a></li>
+              <li className={styles["nav-li"]}><a className={styles["nav-a"]} href="#">Contact</a></li>
+            </ul>
+          </nav>
+          <div className={styles["hero"]}>
+            <h1>Lose weight and stay healthy</h1>
+            <p>Our weight loss app helps you reach your goals and maintain a healthy lifestyle</p>
+            <Link href="/tracker/add-budget">
+                <button className={styles["primary-btn"]}>Get started</button>
+            </Link>
+          </div>
+        </header>
+        <section className={styles["secondary-bg"]}>
+          <div className={styles["container"]}>
+            <div className={styles["text-center"]}>What we do</div>
+            <div className={styles["feature"]}>
+              <Image
+                src="/images/icon1.png" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="Feature 1"
+                className={styles["feature-icon"]}
+              />
+              <div className={styles["feature-content"]}>
+                <h3>Track your progress</h3>
+                <p>Keep track of your weight, measurements, and body fat percentage with our app</p>
+              </div>
+            </div>
+            <div className={styles["feature"]}>
+              <Image
+                src="/images/icon2.png" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="Feature 2"
+                className={styles["feature-icon"]}
+              />
+              <div className={styles["feature-content"]}>
+                <h3>Personalized meal plans</h3>
+                <p>Get personalized meal plans based on your preferences and dietary restrictions</p>
+              </div>
+            </div>
+            <div className={styles["feature"]}>
+              <Image
+                src="/images/community.png" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="Feature 3"
+                className={styles["feature-icon"]}
+              />
+              <div className={styles["feature-content"]}>
+                <h3>Community support</h3>
+                <p>Connect with a community of like-minded individuals for support and motivation</p>
+              </div>
             </div>
           </div>
-          <div className={styles["feature"]}>
-            <img src="icon2.png" alt="Feature 2" className="feature-icon" />
-            <div className="feature-content">
-              <h3>Personalized meal plans</h3>
-              <p>Get personalized meal plans based on your preferences and dietary restrictions</p>
+        </section>
+        <section className={styles["price-section"]}>
+          <div className={styles["price-container"]}>
+            <h2 className={styles["text-center"]}>Pricing</h2>
+            <div className={styles["price-cards"]}>
+              <div className={styles["price"]}>
+                <h3>Basic</h3>
+                <p>$9.99/month</p>
+
+                <button className={styles["secondary-btn"]}>Get started</button>
+              </div>
+              <div className={styles["price"]}>
+                <h3>Premium</h3>
+                <p>$19.99/month</p>
+                <button className={styles["secondary-btn"]}>Get started</button>
+              </div>
+              <div className={styles["price"]}>
+                <h3>Ultimate</h3>
+                <p>$29.99/month</p>
+                <button className={styles["secondary-btn"]}>Get started</button>
+              </div>
             </div>
           </div>
-          <div className={styles["feature"]}>
-            <img src="icon3.png" alt="Feature 3" className="feature-icon" />
-            <div className="feature-content">
-              <h3>Community support</h3>
-              <p>Connect with a community of like-minded individuals for support and motivation</p>
-            </div>
+        </section>
+        <footer className={styles["secondary-bg footer"]}>
+          <div className={styles["container"]}>
+            <p className={styles["footer"]}>&copy; 2023 Weight Loss App. All rights reserved.</p>
           </div>
-        </div>
-      </section>
-      <section className={styles["primary-bg"]}>
-        <div className={styles["container"]}>
-          <h2 className={styles["text-center"]}>Pricing</h2>
-          <div className={styles["price"]}>
-            <h3>Basic</h3>
-            <p>$9.99/month</p>
-            <button className={styles["secondary-btn"]}>Get started</button>
-          </div>
-          <div className="price">
-            <h3>Premium</h3>
-            <p>$19.99/month</p>
-            <button className={styles["secondary-btn"]}>Get started</button>
-          </div>
-          <div className="price">
-            <h3>Ultimate</h3>
-            <p>$29.99/month</p>
-            <button className={styles["secondary-btn"]}>Get started</button>
-          </div>
-        </div>
-      </section>
-      <footer className={styles["secondary-bg footer"]}>
-        <div className={styles["container"]}>
-          <p className={styles["text-center"]}>&copy; 2023 Weight Loss App. All rights reserved.</p>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
     </>
 
   )
